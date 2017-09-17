4ed1d7981f0a0aa668593b945e1d8ea05dd243db
Christopher Moore <christopher.e.moore@gmail.com>
2017-09-17 15:06:11 +0300

    changes to eigenvector generation

--- show_flux_matrix.lua
+++ show_flux_matrix.lua
@@ -3,7 +3,12 @@
 -- idk where to put this, or what it should do
 -- I just wanted a script to create a flux jacobian matrix from tensor index equations
 require 'ext'
-require 'symmath'.setup{MathJax={usePartialLHSForDerivative=true}}
+require 'symmath'.setup()
+
+local MathJax = require 'symmath.tostring.MathJax'
+MathJax.usePartialLHSForDerivative = true
+
+symmath.tostring = MathJax
 
 local alpha = var'\\alpha'
 local beta = var'\\beta'
@@ -31,24 +36,80 @@ local function simplify(expr)
 	return expr
 end
 
+local new_printbr_file, printbr
+do
+	local fileindex = 0
+	local printbr_file
+	new_printbr_file = function()
+		fileindex = fileindex + 1
+		if printbr_file then
+			printbr_file:write(MathJax.footer) 
+			printbr_file:close()
+		end
+		printbr_file = assert(io.open('show_flux_matrix.'..('%05d'):format(fileindex)..'.html', 'w'))
+		printbr_file:write(tostring(MathJax.header))
+	end
+	printbr = function(...)
+		assert(printbr_file)
+		local n = select('#', ...)
+		for i=1,n do
+			printbr_file:write(tostring(select(i, ...)))
+			if i<n then printbr_file:write'\t' end
+		end
+		printbr_file:write'<br>\n'
+		printbr_file:flush()
+	end
+end
+
+new_printbr_file()
 
 -- TODO start with EFE, apply Gauss-Codazzi-Ricci, then automatically recast all higher order derivatives as new variables of 1st derivatives
+printbr[[primitive $\partial_t$ defs]]
 
-local defs = table()
-
-defs:insert( alpha',t':eq( 
+local dt_alpha_def = alpha',t':eq( 
 	- alpha^2 * f * gamma'^ij' * K'_ij' 
 	+ alpha'_,i' * beta'^i' 
-) )
+)
+printbr(dt_alpha_def)
 
--- TODO hyp gamma driver beta in terms of B
-
-defs:insert( gamma'_ij,t':eq( 
+local dt_gamma_def = gamma'_ij,t':eq( 
 	-2 * alpha * K'_ij' 
 	+ gamma'_ij,k' * beta'^k' 
 	+ gamma'_kj' * beta'^k_,i' 
 	+ gamma'_ik' * beta'^k_,j' 
-) )
+)
+printbr(dt_gamma_def) 
+
+--[=[
+printbr[[auxiliary variables]]
+
+local a_def = a'_,k':eq(log(alpha)'_,k')
+local simplified_a_def = a_def()
+printbr(a_def:eq(simplified_a_def:rhs()))
+a_def = simplified_a_def
+
+local dt_a_def = a_def:lhs()',t':eq(a_def:rhs()',t')
+printbr(dt_a_def)
+
+-- TODO here -- distribute ,t -- and don't apply Derivative.visitorHandler.Prune ...
+local ruleIndex = Derivative.rules.Prune:find(nil, function(kv) return next(kv) == 'otherVars' end)
+local push = Derivative.rules.Prune:remove(ruleIndex)
+
+dt_a_def = dt_a_def():replace(function(expr)
+	if expr == a:diff(t) then return a'_,t' end
+end)
+printbr(dt_a_def)
+
+Derivative.rules.Prune:insert(ruleIndex, push)
+--]=]
+
+local defs = table()
+
+defs:insert( dt_alpha_def )
+
+-- TODO hyp gamma driver beta in terms of B
+
+defs:insert( dt_gamma_def )
 defs:insert( a'_k,t':eq( 
 	-alpha * f * gamma'^ij' * K'_ij,k' 
 	- alpha^2 * f:diff(alpha) * a'_k' * gamma'^ij' * K'_ij' 
@@ -96,6 +157,8 @@ printbr('partial derivatives')
 for _,def in ipairs(defs) do
 	printbr(def)
 end
+
+--os.exit()
 
 local TensorRef = require 'symmath.tensor.TensorRef'
 for i=1,#defs do
@@ -235,6 +298,9 @@ local dts = Matrix(allLhs):transpose()
 local dxs = Matrix(allDxs):transpose()
 printbr(dts:eq(A * dxs + b))
 
+--os.exit()
+new_printbr_file()
+
 local lambda = var'\\lambda'
 local n = #A
 local charpoly  = (A - Matrix.identity(n) * lambda):determinant()
@@ -260,21 +326,34 @@ roots are:
 -- try solving it for one particular eigenvector/value
 local gammaUxx = gammaUVars[1][1]
 local lambdas = table{
-	-alpha * sqrt(f * gammaUxx),
-	-alpha * sqrt(gammaUxx),
+	-- the more multiplicity, the easier it is to factor
 	0,
+	-alpha * sqrt(gammaUxx),
 	alpha * sqrt(gammaUxx),
+	-- the less multiplicity, the more terms have to be reduced to identity, the more have to be simplified correctly
+	-- and my solver is choking on any sort of polynomials that it is trying to expand and re-factor.
+	-alpha * sqrt(f * gammaUxx),
 	alpha * sqrt(f * gammaUxx),
 }
 local evs = table()
 local multiplicity = table()
 for _,lambda in ipairs(lambdas) do
+	new_printbr_file()
 	printbr('eigenvalue', lambda)
-
-	local A_minus_lambda_I = ((A - Matrix.identity(n) * lambda))()
 	
+	local A_minus_lambda_I = ((A - Matrix.identity(n) * lambda))()
 	printbr('reducing', A_minus_lambda_I)
-	local sofar, reduce = A_minus_lambda_I:inverse()
+
+	local sofar, reduce = A_minus_lambda_I:inverse(nil, function(AInv, A, i, j, reason)
+		new_printbr_file()
+		printbr('eigenvalue', lambda)	
+		printbr('step', i, j, reason)
+		printbr(A, AInv)
+	end)
+	
+	new_printbr_file()
+	printbr('eigenvalue', lambda)
+	printbr('done inverting:')
 	-- show the gaussian elimination results
 	printbr(sofar, reduce)
 
@@ -317,10 +396,12 @@ for _,lambda in ipairs(lambdas) do
 	printbr('eigenvector:')
 	printbr(ev)
 	evs:insert(ev)
+
 end
 
+new_printbr_file()
 
-local lambdaMat = Matrix.diagonal( table.append(
+local lambdaMat = Matrix.diagonal( table():append(
 	lambdas:map(function(lambda,i)
 		return range(multiplicity[i]):map(function() return lambda end)
 	end):unpack()
@@ -328,12 +409,13 @@ local lambdaMat = Matrix.diagonal( table.append(
 printbr('$\\Lambda$:')
 printbr(lambdaMat)
 
+--[[
 local evMat = Matrix(
-	table.append(
+	table():append(
 		evs:map(function(ev)
-			return ev:transpose()
-		end):unpack()
+		table.unpack(ev:transpose())
 	):unpack()
 ):transpose()
 printbr('R:')
 printbr(evMat)
+--]]
