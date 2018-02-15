302509a274e290ef0cc0ba3c0ff3c5aba4d82571
Christopher Moore <christopher.e.moore@gmail.com>
2018-02-15 08:42:52 +0100

    added C code output for 2008 Yano

--- verify_2008_yano.lua
+++ verify_2008_yano.lua
@@ -10,15 +10,37 @@ Now to verify that the math is correct (I'd decompose it with Mathematica myself
 require 'ext'
 require 'symmath'.setup{MathJax={title='2008 Yano based implementation of Z4'}}
 
+-- enable to verify the left and right eigenvectors work
+local verify = false
+
 local f = var'f'
 local m = var'm'
 local alpha = var'\\alpha'
 
 local xs = table{'x', 'y', 'z'}
 local syms = table{'xx', 'xy', 'xz', 'yy', 'yz', 'zz'}
-local gUxx, gUxy, gUxz, gUyy, gUyz, gUzz = syms:map(function(xij)
+
+local gUvars = syms:map(function(xij)
 	return var('\\gamma^{'..xij..'}')
-end):unpack()
+end)
+local gUxx, gUxy, gUxz, gUyy, gUyz, gUzz = gUvars:unpack()
+local gU = Matrix(
+	{gUxx, gUxy, gUxz},
+	{gUxy, gUyy, gUyz},
+	{gUxz, gUyz, gUzz}
+)
+
+local gLvars = syms:map(function(xij)
+	return var('\\gamma_{'..xij..'}')
+end)
+local gLxx, gLxy, gLxz, gLyy, gLyz, gLzz = gLvars:unpack()
+local gL = Matrix(
+	{gLxx, gLxy, gLxz},
+	{gLxy, gLyy, gLyz},
+	{gLxz, gLyz, gLzz}
+)
+
+local g = var'\\gamma'
 
 local evl = Matrix:zeros{31,31}
 evl[1+0][1+2] = frac(1,2)
@@ -251,7 +273,8 @@ evl[1+30][1+27] = (2 - f * m) / (-2 * gUxx + 2 * f * gUxx)
 evl[1+30][1+28] = sqrt(f) * (-2 + m) / (2 * (-1 + f) * sqrt(gUxx))
 evl[1+30][1+29] = sqrt(f) * gUxy * (-2 + m) / (2 * (-1 + f) * gUxx * sqrt(gUxx))
 evl[1+30][1+30] = sqrt(f) * gUxz * (-2 + m) / (2 * (-1 + f) * gUxx * sqrt(gUxx))
---printbr(var'L':eq(evl))
+clone(evl)
+--printbr(var'L':eq(evl))	-- print L before g_ij substitution
 
 local evr = Matrix:zeros{31,31}
 evr[1+0][1+0] = -2 * gUxz / gUxx
@@ -419,51 +442,52 @@ evr[1+29][1+24] = 1
 evr[1+30][1+0] = 1
 evr[1+30][1+17] = 1
 evr[1+30][1+23] = 1
---printbr(var'R':eq(evr))
+clone(evr)
+--printbr(var'R':eq(evr))	-- print R before g_ij substitution
 
 
 local numErrors = 0
 local n = #evr	-- hope everything is square ...
 
-local evr_evl = ( evr * evl )()
-printbr( (var'R' * var'L'):eq(evr_evl) )
--- [[
-for i=1,n do
-	for j=1,n do
-		local expected = Constant(i == j and 1 or 0)
-		if evr_evl[i][j] ~= expected then
-			printbr('for '..i..','..j..' expected '..expected..' but found '..evr_evl[i][j])
-			for k=1,n do
-				--if evr[i][k] ~= Constant(0) and evl[k][j] ~= Constant(0) then
-					printbr('...influenced by ($R_{1+'..(i-1)..',1+'..(k-1)..'} = $'..evr[i][k]..') * ($L_{1+'..(k-1)..',1+'..(j-1)..'} = $'..evl[k][j]..')')
-				--end
+if verify then
+	local evr_evl = ( evr * evl )()
+	printbr( (var'R' * var'L'):eq(evr_evl) )
+	for i=1,n do
+		for j=1,n do
+			local expected = Constant(i == j and 1 or 0)
+			if evr_evl[i][j] ~= expected then
+				printbr('for '..i..','..j..' expected '..expected..' but found '..evr_evl[i][j])
+				for k=1,n do
+					--if evr[i][k] ~= Constant(0) and evl[k][j] ~= Constant(0) then
+						printbr('...influenced by ($R_{1+'..(i-1)..',1+'..(k-1)..'} = $'..evr[i][k]..') * ($L_{1+'..(k-1)..',1+'..(j-1)..'} = $'..evl[k][j]..')')
+					--end
+				end
+				printbr()
+				numErrors = numErrors + 1
 			end
-			printbr()
-			numErrors = numErrors + 1
 		end
 	end
 end
---]]
 
-local evl_evr = ( evl * evr )()
-printbr( (var'L' * var'R'):eq(evl_evr) )
--- [[
-for i=1,n do
-	for j=1,n do
-		local expected = Constant(i == j and 1 or 0)
-		if evl_evr[i][j] ~= expected then
-			printbr('for '..i..','..j..' expected '..expected..' but found '..evl_evr[i][j])
-			for k=1,n do
-				--if evl[i][k] ~= Constant(0) and evr[k][j] ~= Constant(0) then
-					printbr('...influenced by ($L_{1+'..(i-1)..',1+'..(k-1)..'} = $'..evl[i][k]..') * ($R_{1+'..(k-1)..',1+'..(j-1)..'} = $'..evr[k][j]..')')
-				--end
+if verify then
+	local evl_evr = ( evl * evr )()
+	printbr( (var'L' * var'R'):eq(evl_evr) )
+	for i=1,n do
+		for j=1,n do
+			local expected = Constant(i == j and 1 or 0)
+			if evl_evr[i][j] ~= expected then
+				printbr('for '..i..','..j..' expected '..expected..' but found '..evl_evr[i][j])
+				for k=1,n do
+					--if evl[i][k] ~= Constant(0) and evr[k][j] ~= Constant(0) then
+						printbr('...influenced by ($L_{1+'..(i-1)..',1+'..(k-1)..'} = $'..evl[i][k]..') * ($R_{1+'..(k-1)..',1+'..(j-1)..'} = $'..evr[k][j]..')')
+					--end
+				end
+				printbr()
+				numErrors = numErrors + 1
 			end
-			printbr()
-			numErrors = numErrors + 1
 		end
 	end
 end
---]]
 
 --[[
 local start = os.time()
@@ -486,10 +510,9 @@ local lambdaDiags = range(0,30):map(function(i)
 	if i >= 23 and i <= 28 then return alpha * sqrt(gUxx) end
 	if i == 29 then return -alpha * sqrt(f * gUxx) end
 	if i == 30 then return alpha * sqrt(f * gUxx) end
-	error'here'
 end)
 local Lambda = Matrix.diagonal(lambdaDiags:unpack())
-printbr(var'\\Lambda':eq(Lambda))
+--printbr(var'\\Lambda':eq(Lambda))
 
 
 -- A_ij / alpha
@@ -592,22 +615,22 @@ A_alpha[1+30][1+23] = -gUxx
 A_alpha[1+30][1+25] = -gUxy
 A_alpha[1+30][1+26] = -gUxz
 
-local A_check = (evr * Lambda * evl)()
--- [[
-printbr((var'A' / var'\\alpha'):eq(A_alpha))
-printbr((var'A_{check}' / var'\\alpha'):eq(A_check))
-for i=1,n do
-	for j=1,n do
-		local A_check_ij_alpha = (A_check[i][j] / alpha)()
-		if (A_check_ij_alpha - A_alpha[i][j])() ~= Constant(0) then
-			printbr('$A_{1+'..(i-1)..',1+'..(j-1)..'} / \\alpha = $'..A_check_ij_alpha..' should be '..A_alpha[i][j])
-			numErrors = numErrors + 1
+if verify then
+	local A_check = (evr * Lambda * evl)()
+	printbr((var'A' / alpha):eq(A_alpha))
+	printbr((var'A_{check}' / alpha):eq(A_check))
+	for i=1,n do
+		for j=1,n do
+			local A_check_ij_alpha = (A_check[i][j] / alpha)()
+			if (A_check_ij_alpha - A_alpha[i][j])() ~= Constant(0) then
+				printbr('$A_{1+'..(i-1)..',1+'..(j-1)..'} / \\alpha = $'..A_check_ij_alpha..' should be '..A_alpha[i][j])
+				numErrors = numErrors + 1
+			end
 		end
 	end
 end
---]]
 
-printbr('found '..numErrors..' errors')
+local A = (A_alpha * alpha)()
 
 local Us = table()
 for i,xi in ipairs(xs) do
@@ -626,8 +649,88 @@ for i,xi in ipairs(xs) do
 	Us:insert(var('Z_'..xi))
 end
 local U = Matrix(Us):transpose()
-printbr(var'U':eq(U))
+--printbr(var'U':eq(U))
 
-local F = (A_check * U)()
-printbr(var'F':eq(F))
+local F = (A * U)()
+--printbr(var'F':eq(F))
 
+--[[ TODO verify flux
+-- shiftless time derivatives in the 'l' direction 
+local dt_ai = delta'^r_i' * (alpha * Q)
+local dt_dijk = delta'^r_k' * (alpha * Qij'_ij')
+local dt_Kij = alpha * lambda'^r_ij'
+local dt_Theta = alpha * (D'^r' - E'^r' - Z'^r')
+local dt_Zi = alpha * (-K'^r_i' * delta'^r_i' * (K'_mn' * gamma'^mn' - Theta))
+--]]
+
+printbr('found '..numErrors..' errors')
+printbr()
+printbr()
+
+local detg_gL_def= Matrix(
+	{
+		gUyy * gUzz - gUyz^2,
+		gUxz * gUyz - gUxy * gUzz,
+		gUxy * gUyz - gUxz * gUyy,
+	},
+	{
+		gUxz * gUyz - gUxy * gUzz,
+		gUxx * gUzz - gUxz^2,
+		gUxy * gUxz - gUxx * gUyz,
+	},
+	{
+		gUxy * gUyz - gUxz * gUyy,
+		gUxy * gUxz - gUxx * gUyz,
+		gUxx * gUyy - gUxy^2,
+	}
+)
+
+evl = clone(evl)
+evr = clone(evr)
+for i=1,3 do
+	for j=1,3 do
+		assert(op.sub.is(detg_gL_def[i][j]))
+		local a,b = table.unpack(detg_gL_def[i][j])
+		
+		--printbr(detg_gL_def[i][j], ' => ', g * gL[i][j])
+		evl = evl:replace(a - b, g * gL[i][j])
+		evr = evr:replace(a - b, g * gL[i][j])
+		
+		evl = evl:replace(-b + a, g * gL[i][j])
+		evr = evr:replace(-b + a, g * gL[i][j])
+		
+		evl = evl:replace(b - a, -g * gL[i][j])
+		evr = evr:replace(b - a, -g * gL[i][j])
+		
+		evl = evl:replace(-a + b, -g * gL[i][j])
+		evr = evr:replace(-a + b, -g * gL[i][j])
+	end
+end
+-- print after g_ij substitution
+printbr(var'L':eq(evl))	
+printbr(var'R':eq(evr))
+
+-- output the code to a separate file ...
+local vVars = range(n):map(function(i)
+	return var('input['..(i-1)..']')
+end)
+local vs = Matrix(vVars):T()
+printbr('left eigenvector linear function:')
+local Lv = (evl * vs)()
+local o = assert(io.open('2008_yano.c', 'w'))
+local ToC = require 'symmath.tostring.C'
+local compileVars = table()
+	:append{alpha, f, m, Theta}
+	:append(gLvars)
+	:append(gUvars)
+	:append(vVars)
+for i=1,n do
+	o:write(
+		'result['..(i-1)..'] = '
+		..(ToC:compile( Lv[i][1], compileVars ))
+			:match('{ return (.*); }')
+			:gsub('\\gamma%^{(..)}', function(ij)
+				return 'gammaU.'..ij
+			end)
+		..'\n')
+end
